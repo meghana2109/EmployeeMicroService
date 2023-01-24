40e978aaf0a538ebeeeb5b0ca9801c52d4b89f12
Meghana M <35092448+meghana2109@users.noreply.github.com>
2023-01-24 13:00:41 +0530

    Added a comment

--- src/main/java/com/springboot/microservice/MicroserviceApplication.java
+++ src/main/java/com/springboot/microservice/MicroserviceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MicroserviceApplication {
 //    Class.forName("com.mysql.jdbc.Driver");
-
+//this is main method
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceApplication.class, args);
 	}
